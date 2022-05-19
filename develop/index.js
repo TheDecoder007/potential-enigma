4f6ec01acd3d4ae95412813eeca7b5bcdcec6603
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-19 13:04:39 -0400

    initial questions

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//title, description, installation, usage instructions,
+// credits, languages used, license
+const questions = () => {
+    return inquirer.prompt ([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the title of your project? (Required)',
+          validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('Please enter your project name!');
+              return false
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project (Required)',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you build this project with? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+          type: 'input',
+          name: 'link',
+          message: 'Enter the GitHub link to your project. (Required)',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter the GitHub link!');
+              return false
+            }
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'feature',
+          message: 'Would you like to feature this project?',
+          default: false
+        },
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
