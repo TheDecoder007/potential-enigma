7674a9a3803a380861ffd3def82431bd1f55c9bc
Ryan Murphy <TheDecoder007@gmail.com>
2022-05-19 22:28:25 -0400

    small progress on badge function

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -4,10 +4,10 @@
 
 
 function renderLicenseBadge(License) {
-  
+
 //PROGRESS ON FIRST ONE
   // if (`${License[0]}`) {
-  //   return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  //   return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
   // }
   
 
